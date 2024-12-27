ad4e25a181b5c0d5f395f6f028d13e4460d76409
PmpPiyas <princemahmudpiyas@gmail.com>
2024-12-28 02:43:11 +0600

    Latest Work Added

--- src/Component/MyWork/MyWork.jsx
+++ src/Component/MyWork/MyWork.jsx
@@ -1,11 +1,22 @@
 import "../MyWork/MyWork.css";
 import theme_pattern from "../../assets/theme_pattern.svg";
+import mywork_data from "./../../assets/mywork_data";
+import arrow_icon from "../../assets/arrow_icon.svg";
 export default function MyWork() {
   return (
-    <div>
-      <div className="services-title">
-        <h1>My Services</h1>
+    <div className="mywork">
+      <div className="work-title">
+        <h1>My Latest Work</h1>
         <img src={theme_pattern}></img>
+      </div>
+      <div className="mywork-container">
+        {mywork_data.map((work, idx) => {
+          return <img key={idx} src={work.w_img}></img>;
+        })}
+      </div>
+      <div className="mywork-showmore">
+        <p>Show More</p>
+        <img src={arrow_icon}></img>
       </div>
     </div>
   );
